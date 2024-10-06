cc94fa617bd544d410f82bad6899e5eaf295da24
yokxim <yooksi53@gmail.com>
2024-10-07 08:11:26 +0900

    Entity properties test

--- src/main/java/com/sparta/jpacore/entity/Memo.java
+++ src/main/java/com/sparta/jpacore/entity/Memo.java
@@ -1,4 +1,22 @@
 package com.sparta.jpacore.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity // JPA가 관리할 수 있는 Entity 클래스 지정
+@Table(name = "memo") // 매핑할 테이블의 이름을 지정
+@Setter @Getter
 public class Memo {
+    @Id
+    private Long id;
+
+    // nullable: null 허용 여부
+    // unique: 중복 허용 여부 (false 일때 중복 허용)
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    // length: 컬럼 길이 지정
+    @Column(name = "contents", nullable = false, length = 500)
+    private String contents;
 }
